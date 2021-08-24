6c5e8480f5e689eeed313172b43112bf1c18b51f
hornhuang <hornhuang@tencent.com>
2021-08-24 16:25:21 +0800

    fix:{路由}
    fix:{路由}

--- lib/common/route/route_page.dart
+++ lib/common/route/route_page.dart
@@ -4,6 +4,7 @@ import 'package:hornhuang_github_io/common/route/app_link.dart';
 import 'package:hornhuang_github_io/utils/app_util.dart';
 import 'package:hornhuang_github_io/utils/statement_utils.dart';
 import 'package:hornhuang_github_io/views/about_me/about_me_page.dart';
+import 'package:hornhuang_github_io/views/error_page.dart';
 import 'package:hornhuang_github_io/views/index_page.dart';
 import 'package:hornhuang_github_io/widgets/bottom_nav_bar.dart';
 import 'package:hornhuang_github_io/widgets/enlarge_widget.dart';
@@ -13,7 +14,11 @@ import 'package:hornhuang_github_io/widgets/user_info_card.dart';
 import '../../widgets/swiper_widget.dart';
 
 class RoutePage extends StatefulWidget {
-  static String Route = "/";
+  static String Route = AppLink(
+    pageId: IndexPage.Route,
+    bookId: null,
+    user: null,
+  ).toLocation();
 
   final String? match;
 
@@ -26,18 +31,17 @@ class RoutePage extends StatefulWidget {
 class _RoutePageState extends State<RoutePage> {
 
   Widget _configPage(AppLink link) {
-    print("sadsadad${link.toLocation()}");
     Widget page = case2(link.pageId, {
       IndexPage.Route: IndexPage(link: link),
       AboutMePage.Route: AboutMePage(link: link)
-    }, IndexPage());
+    }, ErrorPage("找不到页面::${link.toLocation()}", null));
     return page;
   }
 
   @override
   Widget build(BuildContext context) {
-    var params = AppLink.fromLocation(widget.match);
-    return _configPage(params);
+    var link = AppLink.fromLocation(widget.match);
+    return _configPage(link);
   }
 
 }
