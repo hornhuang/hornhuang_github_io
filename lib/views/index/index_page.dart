6c0d6d3915d27c2ebaa6ce1a79829c4691e3d9a8
hornhuang <814484626@qq.com>
2021-11-28 16:17:20 +0800

    upd(index): 集成 vm

--- lib/views/index/index_page.dart
+++ lib/views/index/index_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hornhuang_github_io/common/base/base_stateful_widget.dart';
+import 'package:hornhuang_github_io/common/bmob/bmob_api.dart';
 import 'package:hornhuang_github_io/common/route/app_link.dart';
 import 'package:hornhuang_github_io/views/index/view/left_panel.dart';
 import 'package:hornhuang_github_io/views/index/view/right_panel.dart';
+import 'package:hornhuang_github_io/views/index/viewmodel/index_view_model.dart';
+import 'package:provider/provider.dart';
 
 class IndexPage extends StatefulWidget {
   static final String Route = "index";
@@ -17,21 +21,27 @@ class IndexPage extends StatefulWidget {
 class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: LeftPanel()
+    return BaseStatefulWidget<IndexViewModel>(
+      model: IndexViewModel(api: Provider.of(context) ?? BmobApi()),
+      onModelReady: (model) {},
+      builder: (context, model, child) {
+        return Scaffold(
+          body: Row(
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: LeftPanel()
+              ),
+              Expanded(
+                  child: Container(
+                    width: 512,
+                    child: RightPanel(),
+                  )
+              ),
+            ],
           ),
-          Expanded(
-              child: Container(
-                width: 512,
-                child: RightPanel(),
-              )
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/views/index/viewmodel/index_view_model.dart
+++ lib/views/index/viewmodel/index_view_model.dart
@@ -1,5 +1,55 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
-class IndexViewModel {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:hornhuang_github_io/common/base/base_view_model.dart';
+import 'package:hornhuang_github_io/common/bmob/bmob_api.dart';
+
+class IndexViewModel extends BaseViewModel {
+
+  Timer? _timer;
+  int _countdownTime = 0;
+
+  IndexViewModel({required BmobApi api}) : super(api: api);
+
+  Future<void> sendSms(String mobile) async {
+    await api.post(event: "sendSms", params: {"mobile":mobile});
+  }
+
+  Future<bool> login(String mobile, String sms) async {
+    return await api.post(event: "login", params: {"mobile":mobile, "sms":sms}) != null;
+  }
+
+  void startCountdown() {
+    _countdownTime = 60;
+    if (_timer == null) {
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        countdownTime--;
+        if (countdownTime == 0) {
+          cancelTimer();
+        }
+      });
+    }
+  }
+
+  void cancelTimer() {
+    if (_timer != null) {
+      _timer?.cancel();
+      _timer = null;
+    }
+  }
+
+
+  void dispose() {
+    cancelTimer();
+    super.dispose();
+  }
+
+  int get countdownTime => _countdownTime;
+
+  set countdownTime(int value) {
+    _countdownTime = value;
+    notifyListeners();
+  }
 
 }
