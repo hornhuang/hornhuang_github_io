72c35185024965a931f8f3e7aeb23f2b4d0cd9c1
hornhuang <814484626@qq.com>
2021-11-28 15:46:02 +0800

    upd(index): 抽离 title widget 和 左边组件

--- lib/views/index/view/left_panel.dart
+++ lib/views/index/view/left_panel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_widget_cache.dart';
 import 'package:hornhuang_github_io/common/Model/video_item_model.dart';
+import 'package:hornhuang_github_io/views/index/view/title_widget.dart';
 
 class LeftPanel extends StatefulWidget {
   const LeftPanel({Key? key}) : super(key: key);
@@ -16,31 +17,10 @@ class _LeftPanelState extends State<LeftPanel> {
   List<VideoItemModel> _recommendations = <VideoItemModel>[];
 
   Widget _buildTitle(String title) {
-    return Row(
-      children: [
-        Container(
-          width: 8,
-          height: 16,
-          color: Colors.orange,
-        ),
-        Text(
-          "$title",
-          style: TextStyle(
-
-          ),
-        ),
-        Spacer(),
-        Text(
-          "更多",
-          style: TextStyle(
-
-          ),
-        )
-      ],
-    );
+    return TitleWidget(title);
   }
 
-  Widget _buildChoices() {
+  Widget _buildVideos() {
     return ListView.builder(
       itemCount: _choices.length,
       itemBuilder: (BuildContext ctxt, int index) {
@@ -49,7 +29,7 @@ class _LeftPanelState extends State<LeftPanel> {
     );
   }
 
-  Widget _buildRecommendations() {
+  Widget _buildTrends() {
     return GridView.count(
       semanticChildCount: _recommendations.length,
       crossAxisCount: 3,
@@ -62,10 +42,10 @@ class _LeftPanelState extends State<LeftPanel> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        _buildTitle("韭菜精选"),
-        // _buildChoices(),
-        _buildTitle("精品推荐"),
-        // _buildRecommendations(),
+        _buildTitle("【视频】精选教程"),
+        _buildVideos(),
+        _buildTitle("最近动态"),
+        _buildTrends(),
       ],
     );
   }
---
+++ lib/views/index/view/title_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TitleWidget extends StatefulWidget {
+  String title;
+
+  TitleWidget(this.title);
+
+  @override
+  _TitleWidgetState createState() => _TitleWidgetState();
+}
+
+class _TitleWidgetState extends State<TitleWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 8,
+          height: 16,
+          color: Colors.orange,
+        ),
+        Text(
+          "${widget.title}",
+          style: TextStyle(
+
+          ),
+        ),
+        Spacer(),
+        Text(
+          "更多",
+          style: TextStyle(
+
+          ),
+        )
+      ],
+    );
+  }
+}
